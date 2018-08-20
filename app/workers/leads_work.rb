0ebbc53ca17380a63f3ec20a522611d1665f8791
Maverick Tesini <mavericktesini@gmail.com>
2018-08-20 00:46:14 +0000

    automatically backed up by learn

--- app/workers/leads_work.rb
+++ app/workers/leads_work.rb
@@ -2,6 +2,10 @@ class LeadsWorker
   include SideKiq::Worker
 
   def perform(leads_file)
+    CSV.foreach(params[:leads].path, headers: true) do |lead|
+      Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
+    end
+    redirect_to customers_path
   end
 
-end 
+end
